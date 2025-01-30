6f5d0dd4179b9b051887d46fc3d3b9d3a5fb93c0
Oleg-Storozhev <olegstoroshev@gmail.com>
2025-01-30 19:19:43 +0200

    started a project Appstore_API

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 import json
 
 from fastapi import FastAPI, Query
@@ -6,20 +7,20 @@ from fastapi.responses import JSONResponse, FileResponse
 app = FastAPI()
 
 
-@app.get("/test")
-def test():
-    return "API is running!"
+@app.get("/healthcheck")
+async def test():
+    return JSONResponse(content={"message": "API is running!"})
 
 
 @app.get("/get_reviews")
-def get_reviews(app_name: str, country: str = Query('us'), num_reviews: int = Query(100)):
+async def get_reviews(app_name: str, country: str = Query('us'), num_reviews: int = Query(100)):
     reviews = {}
     metrics = {}
     return JSONResponse(content={"reviews": reviews, "metrics": metrics})
 
 
 @app.get("/download_reviews")
-def download_reviews(app_name: str):
+async def download_reviews(app_name: str):
     """Download raw review data as JSON."""
     reviews = {"example": "example"}
     file_path = "reviews.json"
@@ -27,3 +28,7 @@ def download_reviews(app_name: str):
         json.dump(reviews, f)
 
     return FileResponse(file_path, filename="reviews.json")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8001)
