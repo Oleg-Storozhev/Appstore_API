3c346ac77515bdfd10150d585917db3a0ec591e7
Oleg-Storozhev <olegstoroshev@gmail.com>
2025-01-30 20:42:53 +0200

    added app_store_scraper

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 import uvicorn
 import json
 
-from fastapi import FastAPI, Query
+from app_store_scraper import AppStore
+from fastapi import FastAPI
 from fastapi.responses import JSONResponse, FileResponse
 
 app = FastAPI()
+
+
+def fetch_reviews(app_name: str, app_id: str, country: str = "us", num_reviews: int = 100):
+    try:
+        app = AppStore(country='us', app_name=app_name, app_id=app_id)
+        app.review(how_many=num_reviews)
+        reviews = app.reviews
+        if not reviews:
+            return {"error": "No reviews found."}
+
+        processed_reviews = []
+        for review in reviews:
+            processed_reviews.append({
+                "title": review.get("title", "No title"),
+                "rating": review.get("rating", "No rating"),
+                "text": review.get("review", "")
+            })
+
+        return processed_reviews
+    except Exception as e:
+        return {"error": str(e)}
 
 
 @app.get("/healthcheck")
@@ -13,8 +35,8 @@ async def test():
 
 
 @app.get("/get_reviews")
-async def get_reviews(app_name: str, country: str = Query('us'), num_reviews: int = Query(100)):
-    reviews = {}
+async def get_reviews(app_name: str, app_id: str):
+    reviews = fetch_reviews(app_name=app_name, app_id=app_id)
     metrics = {}
     return JSONResponse(content={"reviews": reviews, "metrics": metrics})
 
