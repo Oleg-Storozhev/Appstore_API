ec19d60f50c6a44c1407b284f0d2f3507bd17178
Oleg-Storozhev <olegstoroshev@gmail.com>
2025-02-01 15:11:46 +0200

    Refactor metrics calculator for clarity and efficiency

--- service/ml/metrics_calculator.py
+++ service/ml/metrics_calculator.py
@@ -4,27 +4,31 @@ import pandas as pd
 class MetricsCalculator:
     @staticmethod
     def create_df(reviews: list) -> pd.DataFrame:
-        df = pd.DataFrame(reviews, columns=["title", "rating", "review", "cleaned_text", "sentiment"])
-        return df
+        return pd.DataFrame(reviews, columns=["title", "rating", "review", "cleaned_text", "sentiment"])
 
     @staticmethod
     def calculate_for_rating(df: pd.DataFrame):
         mean_rating = df.rating.mean()
         ratings_distribution_count = df.rating.value_counts()
         ratings_distribution_percentage = df.rating.value_counts(normalize=True) * 100
-        return ratings_distribution_count.to_dict(), ratings_distribution_percentage.to_dict(), mean_rating
+        return ratings_distribution_count.to_dict(), ratings_distribution_percentage.to_dict(), mean_rating,
 
     @staticmethod
     def calculate_for_sentiment(df: pd.DataFrame):
-        sentiment_distribution_count = df.sentiment.value_counts()
+        sentiment_distribution_count = df.sentiment.value_count()
         sentiment_distribution_percentage = df.sentiment.value_counts(normalize=True) * 100
         return sentiment_distribution_count.to_dict(), sentiment_distribution_percentage.to_dict()
 
     @staticmethod
     def get_metrics(reviews: list):
         df = MetricsCalculator.create_df(reviews)
-        answer = dict()
-        answer["ratings_distribution_count"], answer["ratings_distribution_percentage"], answer["mean_rating"] = MetricsCalculator.calculate_for_rating(df)
-        answer["sentiment_distribution_count"], answer["sentiment_distribution_percentage"] = MetricsCalculator.calculate_for_sentiment(df)
+        ratings_count, ratings_percentage, mean_rating = MetricsCalculator.calculate_for_rating(df)
+        sentiment_count, sentiment_percentage = MetricsCalculator.calculate_for_sentiment(df)
 
-        return answer
+        return {
+            "ratings_distribution_count": ratings_count,
+            "ratings_distribution_percentage": ratings_percentage,
+            "mean_rating": mean_rating,
+            "sentiment_distribution_count": sentiment_count,
+            "sentiment_distribution_percentage": sentiment_percentage
+        }
