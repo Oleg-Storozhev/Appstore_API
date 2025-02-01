34d452502a23df2bfdc4a7fd21ad6c12c04e5119
Oleg-Storozhev <olegstoroshev@gmail.com>
2025-02-01 15:14:46 +0200

    Refactor metrics calculator for clarity and efficiency

--- service/ml/metrics_calculator.py
+++ service/ml/metrics_calculator.py
@@ -9,26 +9,22 @@ class MetricsCalculator:
     @staticmethod
     def calculate_for_rating(df: pd.DataFrame):
         mean_rating = df.rating.mean()
-        ratings_distribution_count = df.rating.value_counts()
-        ratings_distribution_percentage = df.rating.value_counts(normalize=True) * 100
-        return ratings_distribution_count.to_dict(), ratings_distribution_percentage.to_dict(), mean_rating,
+        ratings_distribution_count = df.rating.value_counts().to_dict()
+        return mean_rating, ratings_distribution_count
 
     @staticmethod
     def calculate_for_sentiment(df: pd.DataFrame):
-        sentiment_distribution_count = df.sentiment.value_count()
-        sentiment_distribution_percentage = df.sentiment.value_counts(normalize=True) * 100
-        return sentiment_distribution_count.to_dict(), sentiment_distribution_percentage.to_dict()
+        sentiment_distribution_count = df.sentiment.value_counts().to_dict()
+        return sentiment_distribution_count
 
     @staticmethod
     def get_metrics(reviews: list):
         df = MetricsCalculator.create_df(reviews)
-        ratings_count, ratings_percentage, mean_rating = MetricsCalculator.calculate_for_rating(df)
-        sentiment_count, sentiment_percentage = MetricsCalculator.calculate_for_sentiment(df)
+        mean_rating, ratings_count = MetricsCalculator.calculate_for_rating(df)
+        sentiment_count = MetricsCalculator.calculate_for_sentiment(df)
 
         return {
-            "ratings_distribution_count": ratings_count,
-            "ratings_distribution_percentage": ratings_percentage,
             "mean_rating": mean_rating,
+            "ratings_distribution_count": ratings_count,
             "sentiment_distribution_count": sentiment_count,
-            "sentiment_distribution_percentage": sentiment_percentage
         }
